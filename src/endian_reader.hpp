817ea61a13ac632e1d8eaff18a80f8c45355c268
J Henning <jonathanchenning@outlook.com>
2023-11-28 16:26:33 -0500

    split reader into cpp file

--- src/endian_reader.hpp
+++ src/endian_reader.hpp
@@ -1,320 +1,70 @@
 #ifndef __ENDIAN_READER_HPP__
 #define __ENDIAN_READER_HPP__
 
-#include "endian_lib.hpp"
+#include <fstream>
+#include <vector>
+#include <string>
+
+#ifndef ENDIAN_LITTLE
+#define ENDIAN_LITTLE 0
+#endif // ENDIAN_LITTLE
+
+#ifndef ENDIAN_BIG
+#define ENDIAN_BIG 1
+#endif // ENDIAN_LITTLE
+
+#ifndef ENDIAN_ASSERT
+#include <assert.h>
+#define ENDIAN_ASSERT assert
+#endif // ENDIAN_ASSERT
 
 class EndianReader
 {
         std::ifstream _fin;
         int _stream_length;
-        int _position;
         int _endianness;
 
 public:
-        EndianReader(const char* filename, int endianness) : _position(0), _endianness(endianness)
-        {
-                //--- Get file length
+        EndianReader(const char* filename, int endianness);
+        ~EndianReader();
 
-                std::ifstream file(filename, std::ios::binary | std::ios::in | std::ios::ate);
-                int size = -1;
-                if(file)
-                {
-                        file.seekg(0, std::ios::end);
-                        size = file.tellg();
-                }
-                else
-                {
-                        perror(filename);
-                }
-                _stream_length = size;
-                file.close();
+        int GetPosition();
+        void SetPosition(int position);
+        void Close();
 
-                //--- Open stream for reading
+        uint8_t ReadByte();
+        int8_t ReadSByte();
+        std::vector<uint8_t> ReadBytes(int length);
+        std::vector<int8_t> ReadSBytes(int length);
 
-                _fin.open(filename, std::ios::in | std::ios::binary);
-        }
-        ~EndianReader()
-        {
-                Close();
-        }
+        uint16_t ReadUInt16();
+        int16_t ReadInt16();
+        std::vector<uint16_t> ReadUInt16s(int length);
+        std::vector<int16_t> ReadInt16s(int length);
 
-        int GetPosition() { return _fin.tellg(); }
-        void SetPosition(int position)
-        {
-                _position = position;
-                _fin.seekg(position, std::ios::beg);
-        }
+        uint32_t ReadUInt24();
+        int32_t ReadInt24();
+        std::vector<uint32_t> ReadUInt24s(int length);
+        std::vector<int32_t> ReadInt24s(int length);
 
-        int GetLength() { return _stream_length; }
-        int GetEndianness() { return _endianness; }
-        void Close() { _fin.close(); }
+        uint32_t ReadUInt32();
+        int32_t ReadInt32();
+        std::vector<uint32_t> ReadUInt32s(int length);
+        std::vector<int32_t> ReadInt32s(int length);
+
+        float ReadFloat();
+        std::vector<float> ReadFloats(int length);
+
+        double ReadDouble();
+        std::vector<double> ReadDoubles(int length);
+
+        std::string ReadStringNT();
 
 private:
-        //--- Throws an exception if the attempted operation is invalid
 
-        void Try(int length)
-        {
-                if(_position + length > _stream_length)
-                        throw std::invalid_argument("length extends past stream");
-        }
-
-        //--- Reverses a subset of the buffer
-
-        void ReverseBufferSegment(char* buffer, int start, int size)
-        {
-                int end = start + size - 1;
-                while (start < end)
-                {
-                        int temp = buffer[start];
-                        buffer[start] = buffer[end];
-                        buffer[end] = temp;
-                        start++;
-                        end--;
-                }
-        }
-
-        //--- Reads a given amount of bytes into the buffer
-
-        void FillBuffer(char* buffer, const int count, int stride)
-        {
-                _fin.read(buffer, count);
-                _position += count;
-                if(_endianness == ENDIAN_BIG)
-                {
-                        for(int i = 0; i < count; i += stride)
-                                ReverseBufferSegment(buffer, i, stride);
-                }
-        }
-
-public:
-        ////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Bytes              ////////////////
-        ////////////////////////////////////////////////////////////////////////
-
-        uint8_t ReadByte()
-        {
-                Try(1);
-                char buffer[1];
-                FillBuffer(buffer, 1, 1);
-                return (uint8_t)buffer[0];
-        }
-
-        std::vector<uint8_t> ReadBytes(int length)
-        {
-                Try(length);
-                std::vector<uint8_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadByte());
-                return buffer;
-        }
-
-        int8_t ReadSByte()
-        {
-                Try(1);
-                char buffer[1];
-                FillBuffer(buffer, 1, 1);
-                return (int8_t)buffer[0];
-        }
-
-        std::vector<int8_t> ReadSBytes(int length)
-        {
-                Try(length);
-                std::vector<int8_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadSByte());
-                return buffer;
-        }
-
-        ////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Int16              ////////////////
-        ////////////////////////////////////////////////////////////////////////
-
-        uint16_t ReadUInt16()
-        {
-                Try(2);
-                char buffer[2];
-                FillBuffer(buffer, 2, 2);
-                return (uint16_t)((unsigned char)buffer[1] << 8) |
-                        (unsigned char)buffer[0];
-        }
-
-        std::vector<uint16_t> ReadUInt16s(int length)
-        {
-                Try(length * sizeof(uint16_t));
-                std::vector<uint16_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadUInt16());
-                return buffer;
-        }
-
-        int16_t ReadInt16()
-        {
-                Try(2);
-                char buffer[2];
-                FillBuffer(buffer, 2, 2);
-                return (uint16_t)((unsigned char)buffer[1] << 8) |
-                        (unsigned char)buffer[0];
-        }
-
-        std::vector<int16_t> ReadInt16s(int length)
-        {
-                Try(length * sizeof(int16_t));
-                std::vector<int16_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadInt16());
-                return buffer;
-        }
-
-        ////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Int24              ////////////////
-        ////////////////////////////////////////////////////////////////////////
-
-        uint32_t ReadUInt24()
-        {
-                Try(3);
-                char buffer[3];
-                FillBuffer(buffer, 3, 3);
-                return (uint32_t)((unsigned char)buffer[2] << 16) |
-                        ((unsigned char)buffer[1] << 8) |
-                        (unsigned char)buffer[0];
-        }
-
-        std::vector<uint32_t> ReadUInt24s(int length)
-        {
-                Try(length * 3);
-                std::vector<uint32_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadUInt24());
-                return buffer;
-        }
-
-        int32_t ReadInt24()
-        {
-                Try(3);
-                char buffer[3];
-                FillBuffer(buffer, 3, 3);
-                return (int32_t)((unsigned char)buffer[2] << 16) |
-                        ((unsigned char)buffer[1] << 8) |
-                        (unsigned char)buffer[0];
-        }
-
-        std::vector<int32_t> ReadInt24s(int length)
-        {
-                Try(length * 3);
-                std::vector<int32_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadInt24());
-                return buffer;
-        }
-
-        ////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Int32              ////////////////
-        ////////////////////////////////////////////////////////////////////////
-
-        uint32_t ReadUInt32()
-        {
-                Try(4);
-                char buffer[4];
-                FillBuffer(buffer, 4, 4);
-
-                return (uint32_t)((unsigned char)buffer[3] << 24) |
-                        ((unsigned char)buffer[2] << 16) |
-                        ((unsigned char)buffer[1] << 8) |
-                        (unsigned char)buffer[0];
-        }
-
-        std::vector<uint32_t> ReadUInt32s(int length)
-        {
-                Try(length * sizeof(uint32_t));
-                std::vector<uint32_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadUInt32());
-                return buffer;
-        }
-
-        int32_t ReadInt32()
-        {
-                Try(4);
-                char buffer[4];
-                FillBuffer(buffer, 4, 4);
-                return (int32_t)((unsigned char)buffer[3] << 24) |
-                        ((unsigned char)buffer[2] << 16) |
-                        ((unsigned char)buffer[1] << 8) |
-                        (unsigned char)buffer[0];
-        }
-
-        std::vector<int32_t> ReadInt32s(int length)
-        {
-                Try(length * sizeof(int32_t));
-                std::vector<int32_t> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadInt32());
-                return buffer;
-        }
-
-        ////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Float              ////////////////
-        ////////////////////////////////////////////////////////////////////////
-
-        float ReadFloat()
-        {
-                Try(4);
-                char buffer[4];
-                FillBuffer(buffer, 4, 4);
-
-                float out;
-                memcpy(&out, buffer, sizeof out);
-                return out;
-        }
-
-        std::vector<float> ReadFloats(int length)
-        {
-                Try(length * sizeof(float));
-                std::vector<float> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadFloat());
-                return buffer;
-        }
-
-        /////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Double              ////////////////
-        /////////////////////////////////////////////////////////////////////////
-
-        double ReadDouble()
-        {
-                Try(8);
-                char buffer[8];
-                FillBuffer(buffer, 8, 8);
-
-                double out;
-                memcpy(&out, buffer, sizeof out);
-                return out;
-        }
-
-        std::vector<double> ReadDoubles(int length)
-        {
-                Try(length * sizeof(double));
-                std::vector<double> buffer;
-                for(int i = 0; i < length; i++)
-                        buffer.push_back(ReadDouble());
-                return buffer;
-        }
-
-        //////////////////////////////////////////////////////////////////////////
-        ////////////////                Read Strings              ////////////////
-        //////////////////////////////////////////////////////////////////////////
-
-        std::string ReadStringNT()
-        {
-                std::string out = "";
-                char letter = ReadByte();
-                while(letter != 0)
-                {
-                        out += letter;
-                        letter = ReadByte();
-                }
-                return out;
-        }
+        void Try(int length);
+        void ReverseBufferSegment(char* buffer, int start, int size);
+        void FillBuffer(char* buffer, const int count, int stride);
 };
 
-#endif
+#endif // __ENDIAN_READER_HPP__
